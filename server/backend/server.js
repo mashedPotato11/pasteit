b73d94010632fd713b9dceb97976a1c0e25e09bd
Anmol <a.mahajan110104@gmail.com>
2023-07-12 21:18:46 +0530

    changes

--- server/backend/server.js
+++ server/backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     allowedHeaders: ["Content-Type"],
-    origin: ["http://localhost:3000"],
+    origin: ["https://pasteit-now.vercel.app/"],
   })
 );
 app.use(express.json());
